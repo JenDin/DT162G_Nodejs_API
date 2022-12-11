8597cfd3038e06325d354443b391d2f29c8863fc
Jenzi <jennydinh94@gmail.com>
2022-12-12 00:09:01 +0100

    Model timestamps changed

--- menu_api/models/Booking.js
+++ menu_api/models/Booking.js
@@ -1,40 +1,36 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const bookingSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
+const bookingSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    guestAmount: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  guestAmount: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    required: true,
-  },
-  time: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    immutable: true,
-    default: () => Date.now(),
-  },
-  updatedAt: {
-    type: Date,
-    default: () => Date.now(),
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Booking", bookingSchema);
--- menu_api/models/Dish.js
+++ menu_api/models/Dish.js
@@ -1,32 +1,28 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const dishSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const dishSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
   },
-  category: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    immutable: true,
-    default: () => Date.now(),
-  },
-  updatedAt: {
-    type: Date,
-    default: () => Date.now(),
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Dish", dishSchema);
